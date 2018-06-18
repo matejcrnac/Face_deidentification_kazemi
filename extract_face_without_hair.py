b0cee0c4efc43811755836ffca5748d54193f7b8
Matej Crnac <matej.crnac@gmail.com>
2018-06-18 19:45:48 +0200

    implemented extracting faces withoud hair from original and deidentified images

---
+++ extract_face_without_hair.py
@@ -0,0 +1,60 @@
+import dlib
+import cv2
+import numpy
+
+from FacialLandmarkDetection import *
+
+def draw_convex_hull(im, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+
+FEATHER_AMOUNT = 3
+
+def get_face_mask(im, landmarks):
+    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+
+    draw_convex_hull(im,
+                     landmarks,
+                     color=1)
+
+    im = numpy.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
+
+
+data_folder_deidentification = "/media/matej/D/Diplomski/baza/rezultati/deidentification_destination_all_images_all_k_no_comparing_landmar_distances_spec"
+
+if __name__ == "__main__":
+    image_name = "/media/matej/D/Diplomski/baza/rezultati/deidentification_destination_all_images_all_k_no_comparing_landmar_distances_spec/001/001_k_5.ppm"
+    detector = FacialLandmarkDetector(image_name)
+    #detector.detect_frontal_face(True)
+    #detector.showImage()
+    image = detector.getImage()
+
+    parts = detector.detectFacialLandmarks(draw = False, normalize=False, numpy_format = True)
+    
+    [[max_x,max_y]] = parts.max(axis=0).tolist()
+    [[min_x,min_y]] = parts.min(axis=0).tolist()
+    parts = numpy.vstack([parts, [(int)((max_x+min_x)/2), min_y-10]])
+    parts = numpy.vstack([parts, [(int)(((max_x+min_x)/2 + min_x)/2), min_y-10]])
+    parts = numpy.vstack([parts, [(int)(((max_x+min_x)/2 + max_x)/2), min_y-10]])
+    #parts = numpy.vstack([parts, [(int)((max_x+min_x)/2 + (max_x+min_x)/4), min_y-10]])
+    #parts = numpy.vstack([parts, [(int)((max_x+min_x)/2 + (max_x+min_x)/3), min_y-10]])
+    
+    mask = get_face_mask(image, parts)
+    
+    image = (image * mask).astype(numpy.uint8)
+    #cv2.imshow("bez", image)
+    #cv2.waitKey(0)
+    image = image[min_y - 20:max_y + 10, min_x - 10:max_x + 10]
+    cv2.imshow("bez", image)
+    cv2.waitKey(0)
+    #print(parts)
+    #foundParts = detector.getFacialLandmarksOfFacePart(["Nose", "Mouth"], True)
+    #detector.showImage()
+    #print(foundParts)
+    #ROI = detector.extractFacePart("EyeRegion")
+    #cv2.imshow('image',ROI)
---
+++ extract_face_without_hair_deidentified_images.py
@@ -0,0 +1,82 @@
+import dlib
+import cv2
+import numpy
+import os
+
+from FacialLandmarkDetection import *
+
+def draw_convex_hull(im, points, color):
+    points = cv2.convexHull(points)
+    cv2.fillConvexPoly(im, points, color=color)
+
+FEATHER_AMOUNT = 3
+
+def get_face_mask(im, landmarks):
+    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+
+    draw_convex_hull(im,
+                     landmarks,
+                     color=1)
+
+    im = numpy.array([im, im, im]).transpose((1, 2, 0))
+
+    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+
+    return im
+def get_images_Path(folder, extension):
+    images_paths = []
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            if file.endswith(extension):
+                fName = os.path.join(root, file)
+                images_paths.append(fName)
+
+    return sorted(images_paths)
+
+
+data_folder_deidentification = "/media/matej/D/Diplomski/baza/rezultati/deidentification_destination_all_images_all_k_no_comparing_landmar_distances_spec"
+
+destination_folder = "/media/matej/D/Diplomski/baza/rezultati"
+if __name__ == "__main__":
+    
+    images_paths = get_images_Path(data_folder_deidentification, ".ppm")
+    
+    destination = destination_folder + "/deidentification_destination_all_images_all_k_no_comparing_landmar_distances_spec_extracted_" + "/"
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for image_name in images_paths:
+        detector = FacialLandmarkDetector(image_name)
+        #detector.detect_frontal_face(True)
+        #detector.showImage()
+        image = detector.getImage()
+
+        parts = detector.detectFacialLandmarks(draw = False, normalize=False, numpy_format = True)
+        
+        [[max_x,max_y]] = parts.max(axis=0).tolist()
+        [[min_x,min_y]] = parts.min(axis=0).tolist()
+        parts = numpy.vstack([parts, [(int)((max_x+min_x)/2), min_y-10]])
+        parts = numpy.vstack([parts, [(int)(((max_x+min_x)/2 + min_x)/2), min_y-10]])
+        parts = numpy.vstack([parts, [(int)(((max_x+min_x)/2 + max_x)/2), min_y-10]])
+        #parts = numpy.vstack([parts, [(int)((max_x+min_x)/2 + (max_x+min_x)/4), min_y-10]])
+        #parts = numpy.vstack([parts, [(int)((max_x+min_x)/2 + (max_x+min_x)/3), min_y-10]])
+        
+        mask = get_face_mask(image, parts)
+        
+        image = (image * mask).astype(numpy.uint8)
+        #cv2.imshow("bez", image)
+        #cv2.waitKey(0)
+        image = image[min_y - 20:max_y + 10, min_x - 10:max_x + 10]
+        
+        name = image_name.split("/")[-1].split("_")[0]
+        k = image_name.split("/")[-1].split("_")[2].split(".")[0]
+        destination_new = destination + name + "/"
+        if not os.path.exists(destination_new):
+            os.makedirs(destination_new)
+        imname = destination_new + name + "_k_" + str(k) + ".ppm"
+        
+        cv2.imwrite(imname, image)
+        
+        print("Image: " + name + ", k = " + str(k))
+        del detector
